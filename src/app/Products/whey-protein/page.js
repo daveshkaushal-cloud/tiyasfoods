741ca19c684135bf8c69bc6edc7eba79878aa2d6
daveshkaushal-cloud <daveshkaushal@gmail.com>
2026-09-08 03:36:14 +0530

    Add whey protein product page

---
+++ src/app/Products/whey-protein/page.js
@@ -0,0 +1,10 @@
+import ProductExperience from "@/app/Components/ProductExperience/ProductExperience";
+
+export const metadata = {
+  title: "Whey Protein | TIYAS FOOD",
+  description: "Explore TIYAS FOOD Whey Protein and add it to your cart.",
+};
+
+export default function WheyProteinPage() {
+  return <ProductExperience type="whey" />;
+}
